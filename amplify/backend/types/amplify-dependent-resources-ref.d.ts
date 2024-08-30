876b19a686a3d61e0ee44aa364e3d411150f9b03
jquinteb <juandavid.quintero@gmail.com>
2024-08-29 23:45:46 -0500

    sube archivo aws faltante

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "retomeliebb0b9d3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
