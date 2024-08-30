28b028fdd3bbb7d0eaa7231b84abf6dcf47b3bc4
jquinteb <juandavid.quintero@gmail.com>
2024-08-29 22:53:32 -0500

    added authenticator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import TableList from './components/TableList';
 import TableCrud from './components/TableCrud';
-import './styles.css';  // Importa el archivo de estilos consolidado
+import './styles.css'; 
+
+
+import {Authenticator} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 
 const App = () => {
   const [selectedTable, setSelectedTable] = useState(null);
 
   return (
-    <div>
-      <h1>CRUD Application</h1>
-      <div className="container">
-        <TableList onTableSelect={setSelectedTable} />
-        {selectedTable && <TableCrud table={selectedTable} />}
-      </div>
-    </div>
-  );
+    <Authenticator>    
+        <div>          
+          <h1>CRUD Application</h1>
+          <div className="container">
+            <TableList onTableSelect={setSelectedTable} />
+            {selectedTable && <TableCrud table={selectedTable} />}
+
+            {({ signOut, user }) => (
+              <main>
+                <h1>Hello, {user.username}</h1>
+                <button onClick={signOut}>Sign out</button>
+              </main>
+            )}
+
+          </div>
+        </div>        
+        </Authenticator>
+    );
+
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports'; 
+
 import './styles.css';
 import App from './App';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
